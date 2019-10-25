add6bdbd8abfa51ac190afbfca915d7c02c83461
Aye Nyein Aung <52395750+ryderneo@users.noreply.github.com>
2019-10-25 08:59:08 +0630

    Excel updated

---
+++ PlayBooks_Customized4AGB/PythonCodes/Excel.py
@@ -0,0 +1,16 @@
+from pandas import ExcelWriter
+import glob
+import os
+import pandas as pd
+
+writer = ExcelWriter("output.xlsx")
+
+for filename in glob.glob("*.xlsx"):
+    excel_file = pd.ExcelFile(filename)
+    (_, f_name) = os.path.split(filename)
+    (f_short_name, _) = os.path.splitext(f_name)
+    for sheet_name in excel_file.sheet_names:
+        df_excel = pd.read_excel(filename, sheet_name=sheet_name)
+        df_excel.to_excel(writer, f_short_name+'_'+sheet_name, index=False)
+
+writer.save()
